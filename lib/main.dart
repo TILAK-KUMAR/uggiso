10f8334cabfd874c023aa8276ddc3bb5000d7193
Tilak <tilak.rk@pharmarack.com>
2024-06-03 15:34:59 +0530

    Firebase Error resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,22 +7,32 @@ import 'package:uggiso/Bloc/SignUpBloc/signup_bloc.dart';
 import 'package:uggiso/Bloc/VerifyOtpBloc/VerifyOtpBloc.dart';
 import 'package:uggiso/app_routes.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky, overlays: [SystemUiOverlay.bottom],);
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCXfSKnMi_jvtwDIDT4AD9JxoKwJuzWkfQ',
+          appId: '1:741537959124:android:ee8a4ae8c6ee76cfd2bf41',
+          messagingSenderId: '9964367047',
+          projectId: 'uggiso-469cc'));
+  /*try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+  }*/
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.immersiveSticky,
+    overlays: [SystemUiOverlay.bottom],
+  );
 
-  runApp(
-    MultiBlocProvider(
-        providers: [
-          BlocProvider<SignUpBloc>(
-            create: (context) => SignUpBloc(),
-          ),
-          BlocProvider<VerifyOtpBloc>(
-            create: (context) => VerifyOtpBloc(),
-          ),
-        ], child: const MyApp()));
-
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider<SignUpBloc>(
+      create: (context) => SignUpBloc(),
+    ),
+    BlocProvider<VerifyOtpBloc>(
+      create: (context) => VerifyOtpBloc(),
+    ),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -37,15 +48,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         pageTransitionsTheme: const PageTransitionsTheme(
           builders: {
-            TargetPlatform.android: const CupertinoPageTransitionsBuilder(), // For Android
-            TargetPlatform.iOS: const CupertinoPageTransitionsBuilder(), // For iOS
+            TargetPlatform.android: const CupertinoPageTransitionsBuilder(),
+            // For Android
+            TargetPlatform.iOS: const CupertinoPageTransitionsBuilder(),
+            // For iOS
           },
         ),
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.initialRoute,
       onGenerateRoute: AppRoutes.generateRoute,
-     /* onLaunch: (Map<String, dynamic> message) async {
+      /* onLaunch: (Map<String, dynamic> message) async {
         print('onLaunch: $message');
         // Handle the notification
       },
